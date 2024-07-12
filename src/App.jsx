45fa212f91a059c23437c771cd1593c44846bcde
Kristina Bekher <bekherkr@gmail.com>
2024-07-12 22:39:40 +0200

    Initial folder setup, vars, fonts, utils etc

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1>
+       Creating Visual Solutions that Inspire the World
+       </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
