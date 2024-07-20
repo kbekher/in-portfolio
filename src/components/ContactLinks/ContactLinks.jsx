2184ccc11377cdffe31368d5be9a91b354bc14aa
Kristina Bekher <bekherkr@gmail.com>
2024-07-20 18:40:25 +0200

    Adapt footer to different screen sizes

--- src/components/ContactLinks/ContactLinks.jsx
+++ src/components/ContactLinks/ContactLinks.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
-import { MEDIA_LINKS }from '../../constants/index';
+import { MEDIA_LINKS } from '../../constants/index';
 
 import './ContactLinks.scss';
 
 export const ContactLinks = () => {
   return (
     <div className="ContactLinks">
-      {MEDIA_LINKS.map(({ name, href }) => (
-         <a
-         key={name}
-         href={href}
-         target="_blank"
-         rel="noreferrer"
-         title={`Ivan's ${name}`}
-         className={`ContactLinks__link ContactLinks__link--${name}`}
-       >
-         <i className={`fa-brands fa-${name}`}></i>
-       </a>
-      ))}
+      <div className="ContactLinks__content">
+        {MEDIA_LINKS.map(({ name, href }) => (
+          <a
+            key={name}
+            href={href}
+            target="_blank"
+            rel="noreferrer"
+            title={`Ivan's ${name}`}
+            className={`ContactLinks__link ContactLinks__link--${name}`}
+          >
+            <i className={`fa-brands fa-${name}`}></i>
+          </a>
+        ))}
+      </div>
     </div>
   );
 }
