737f0e9cac02f42059fab31a743d77b5434eb7f6
Kristina Bekher <bekherkr@gmail.com>
2024-07-20 16:48:39 +0200

    Refactor, adjust to screen resolutions part 1

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Spline from '@splinetool/react-spline';
 import { useTranslation } from "react-i18next";
 
@@ -18,61 +18,64 @@ const Header = () => {
   }
 
   // TODO: check if height is changed on scroll
-  const [viewportHeight, setViewportHeight] = useState(window.innerHeight);
+  // const [viewportHeight, setViewportHeight] = useState(window.innerHeight);
 
-  useEffect(() => {
-    const handleResize = () => {
-      setViewportHeight(window.innerHeight);
-    };
+  // useEffect(() => {
+  //   const handleResize = () => {
+  //     setViewportHeight(window.innerHeight);
+  //   };
 
-    // Set initial height
-    setViewportHeight(window.innerHeight);
+  //   // Set initial height
+  //   setViewportHeight(window.innerHeight);
 
-    // Update height on window resize
-    window.addEventListener('resize', handleResize);
+  //   // Update height on window resize
+  //   window.addEventListener('resize', handleResize);
 
-    // Cleanup listener on component unmount
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
+  //   // Cleanup listener on component unmount
+  //   return () => {
+  //     window.removeEventListener('resize', handleResize);
+  //   };
+  // }, []);
 
   return (
-    <header className="Header" style={{ height: `${viewportHeight}px` }} >
-      <div className="Header__content">
+    // <header className="Header" style={{ height: `${viewportHeight}px` }} >
+    <header className="Header">
 
         <div className="Header__visualContent">
           <Spline scene="https://prod.spline.design/liDjqaY-i3BfRC2o/scene.splinecode" />
+
         </div>
 
         <Hello isLayerOpen={isLayerOpen} setIsLayerOpen={setIsLayerOpen} />
 
         <div className="Header__textContent">
-          <div className="Header__textContent-inner">
+          <div className="container">
+            <div className="Header__textContent-inner">
 
-            <div className="Header__top">
-              <Logo />
+              <div className="Header__top">
+                <Logo />
 
-              <h1 className='Header__title'>{t("title")}</h1>
-            </div>
-
-            <div className="Header__controls">
-              <div className="Header__nav">
-                <Navigation />
+                <h1 className='Header__title'>{t("title")}</h1>
               </div>
 
-              <div className="Header__actions">
-                <button type="button" className="Header__connect-btn" onClick={toggleLayer}>
-                  {t("contact")}
-                </button>
+              <div className="Header__controls">
+                <div className="Header__nav">
+                  <Navigation />
+                </div>
 
-                <LanguageToggle />
+                <div className="Header__actions">
+                  <button type="button" className="Header__connect-btn" onClick={toggleLayer}>
+                    {t("contact")}
+                  </button>
+
+                  <LanguageToggle />
+                </div>
               </div>
-            </div>
 
+            </div>
           </div>
+
         </div>
-      </div>
     </header>
   );
 }
--- src/components/Hello/Hello.jsx
+++ src/components/Hello/Hello.jsx
@@ -17,18 +17,20 @@ const Hello = ({ isLayerOpen, setIsLayerOpen }) => {
 
   return (
     <div className={`Hello animate__animated ${isLayerOpen ? "animate__fadeIn" : ""}`}>
-      <div className="Hello__content">
-        <button type="button" className="Hello__closeBtn" onClick={closeLayer}>
-          <svg width="46" height="46" viewBox="0 0 61 61" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M20.3369 20.5576L40.299 38.7377" stroke="#FFFFFE" strokeLinecap="round" />
-            <path d="M21.2305 39.6254L39.4106 19.6633" stroke="#FFFFFE" strokeLinecap="round" />
-            <rect x="0.5" y="0.5" width="60" height="60" rx="30" stroke="#FFFFFE" />
-          </svg>
-        </button>
+      <div className="container">
+        <div className="Hello__content">
+          <button type="button" className="Hello__closeBtn" onClick={closeLayer}>
+            <svg width="38" height="38" viewBox="0 0 61 61" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M20.3369 20.5576L40.299 38.7377" stroke="#FFFFFE" strokeLinecap="round" />
+              <path d="M21.2305 39.6254L39.4106 19.6633" stroke="#FFFFFE" strokeLinecap="round" />
+              <rect x="0.5" y="0.5" width="60" height="60" rx="30" stroke="#FFFFFE" />
+            </svg>
+          </button>
 
-        <div className="Hello__textContent">
-          <p className="Hello__text">{t("hello")}</p>
-          <a href="mailto:inozemtsevco@gmail.com" className="Hello__email">inozemtsevco@gmail.com</a>
+          <div className="Hello__textContent">
+            <p className="Hello__text">{t("hello")}</p>
+            <a href="mailto:inozemtsevco@gmail.com" className="Hello__email">inozemtsevco@gmail.com</a>
+          </div>
         </div>
       </div>
     </div>
